bc2a17b6d4f1c49b51cb86f26998d5e101be4cd0
ivanearisty <iae225@stern.nyu.edu>
2025-04-01 19:36:12 -0400

    Pyproject dependency changes + removed message double definitions
    Message does not depend on itself.
    Inbox client depends on message.
    Inbox client should not redifine message in its protocol implementation,
    it should import it.

--- src/inbox_client_api/src/__init__.py
+++ src/inbox_client_api/src/__init__.py
@@ -1,39 +1,5 @@
 from typing import Iterator, Protocol
-
-
-class Message(Protocol):
-    """A Mail Message."""
-
-    @property
-    def id(self) -> str:
-        """Return the id of the message."""
-        raise NotImplementedError()
-
-    @property
-    def from_(self) -> str:
-        """Return the sender of the message."""
-        raise NotImplementedError()
-
-    @property
-    def to(self) -> str:
-        """Return the recipient of the message."""
-        raise NotImplementedError()
-
-    @property
-    def date(self) -> str:
-        """Return the date of the message."""
-        raise NotImplementedError()
-
-    @property
-    def subject(self) -> str:
-        """Return the subject of the message."""
-        raise NotImplementedError()
-
-    @property
-    def body(self) -> str:
-        """Return the body of the message."""
-        raise NotImplementedError()
-
+from message import Message
 
 class Client(Protocol):
     """Inbox Client Interface Protocol."""
