521b0f69273c324cc576f76c33ed1e227c299320
kc34251 <kc4433@nyu.edu>
2025-04-12 19:10:32 -0400

    Initial attempt for implementation of client and message

--- src/inbox_client_api/src/inbox_client_api/__init__.py
+++ src/inbox_client_api/src/inbox_client_api/__init__.py
@@ -24,53 +24,6 @@ class Client(Protocol):
         """
         raise NotImplementedError
 
-    def send_message(self, to: str, subject: str, body: str) -> bool:
-        """
-        Send a message to the specified recipient.
-
-        Parameters:
-            to (str): The email address of the recipient.
-            subject (str): The subject line of the message.
-            body (str): The content of the message.
-
-        Returns:
-            bool: True if the message was sent successfully, False otherwise.
-
-        Raises:
-            NotImplementedError: If the method is not implemented by the subclass.
-        """
-        raise NotImplementedError
-
-    def delete_message(self, message_id: str) -> bool:
-        """
-        Delete a message by its unique identifier.
-
-        Parameters:
-            message_id (str): The unique ID of the message to be deleted.
-
-        Returns:
-            bool: True if the message was deleted successfully, False otherwise.
-
-        Raises:
-            NotImplementedError: If the method is not implemented by the subclass.
-        """
-        raise NotImplementedError
-
-    def mark_as_read(self, message_id: str) -> bool:
-        """
-        Mark a message as read using its unique identifier.
-
-        Parameters:
-            message_id (str): The unique ID of the message to be marked as read.
-
-        Returns:
-            bool: True if the message was successfully marked as read, False otherwise.
-
-        Raises:
-            NotImplementedError: If the method is not implemented by the subclass.
-        """
-        raise NotImplementedError
-
 def get_client() -> Client:
     """
     Return an instance of a Mail Client.
