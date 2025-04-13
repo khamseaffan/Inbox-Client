17b3d307e4b9b8a2055c6d824e7c3a450fb3c48d
Affan Khamse <khamseaffan@gmail.com>
2025-04-13 00:00:44 -0400

    corrected

--- src/email_analysis_impl/src/email_analysis_impl/__init__.py
+++ src/email_analysis_impl/src/email_analysis_impl/__init__.py
@@ -2,8 +2,5 @@ import email_analysis_api  # Correct package name for the API
 from . import _impl
 
 
-if hasattr(email_analysis_api, 'get_analyzer'):
-    email_analysis_api.get_analyzer = _impl.get_analyzer
-    print("DI: Injected _impl.get_analyzer into email_analysis_api.get_analyzer")
-else:
-    print("DI ERROR: email_analysis_api does NOT have get_analyzer attribute before DI assignment!")
+email_analysis_api.get_analyzer = _impl.get_analyzer
+    
